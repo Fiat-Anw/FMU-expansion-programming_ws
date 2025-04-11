492965494f16d7ee9ea6f282143e0643398142e4
Fiat-Anw <fiat.anw@gmail.com>
2025-04-12 00:44:01 +0700

    Make BMP581 to be class with argument i2c_bus

--- Test/baro/BMP581.py
+++ Test/baro/BMP581.py
@@ -2,11 +2,13 @@ from BMP581_DRIVE import BMP581  # Import the BMP581 class from the BMP581_DRIVE
 import time
 
 try:
-    sensor = BMP581()  # Initialize the sensor
+    sensor = BMP581(i2c_bus=1)  # Initialize the sensor
     while True:
         pressure, temperature = sensor.read_sensor_data()
         if pressure is not None and temperature is not None:
-            print(f"Pressure: {pressure} Pa, Temperature: {temperature} °C")
+            print(f"Pressure: {pressure/1000} kPa, Temperature: {temperature} °C")
+        else:
+            print("Failed to read sensor data.")
         time.sleep(1)  # Delay between readings
 
 except KeyboardInterrupt:
